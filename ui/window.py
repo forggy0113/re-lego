87b98282e58552515ef20e6df67ab24c10d1858e
Rui1209 <sunji410224@gmail.com>
2024-08-04 22:40:21 +0800

    [add & delete]sign in ui design

--- ui/window.py
+++
@@ -1,8 +0,0 @@
-# my_window.py
-from PyQt6.QtWidgets import QMainWindow
-from signin_inference_ui import Ui_MainWindow
-
-class MyWindow(QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self)
