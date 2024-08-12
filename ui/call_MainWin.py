6976491dc6961955ea789595c6e0643ffbad79ae
Rui1209 <sunji410224@gmail.com>
2024-08-13 00:53:22 +0800

    [add]main_ui

--- ui/call_MainWin.py
+++ ui/call_MainWin.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt6.QtWidgets import QMainWindow, QApplication, QLineEdit
-from password_ui import Ui_MainWindow  # 變更匯入py檔，顯示主畫面
+from main_ui import Ui_MainWindow  # 變更匯入py檔，顯示主畫面
 
 class MyMainWin:
     def __init__(self):
@@ -11,18 +11,6 @@ class MyMainWin:
         # 設置用戶介面
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self.main_window)
-        # 變更背景色
-        self.main_window.setStyleSheet(
-            """
-            QMainWindow {
-            background-color: #FCFAED;
-            }
-            QLineEdit {
-                background-color: white;
-            }
-
-            """
-        )
 
     def run_main(self):
         # 顯示介面&結束介面
