89607b3a3c25704f143feb64bf68e652f8e7cdf1
forggy0113 <forggy0113@gmail.com>
2024-12-24 06:28:00 -0800

    modified:   src/sql_py/func_sql.py 	new file:   ui/.Ui_login_resigert.py.swp 	modified:   ui/Ui_face.py 	modified:   ui/Ui_login_resigert.py 	modified:   ui/Ui_teacher_mode.py 	modified:   ui/face.ui 	modified:   ui/login_resigert.ui 	modified:   ui_main.py

--- src/sql_py/func_sql.py
+++ src/sql_py/func_sql.py
@@ -7,7 +7,7 @@ import os
 import qrcode
 from PIL import ImageDraw, ImageFont
     
-from PyQt6.QtCore import QTimer
+from PyQt5.QtCore import QTimer
 class Stus:
     def __init__(self, db, ui,main_window):  # 只接受 db 和 ui 兩個參數
         self.db = db
